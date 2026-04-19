5829defde68d3498c6aadc361cf093679eaae016
tsansch <tristane@umich.edu>
2026-04-18 21:08:27 -0400

    Draw board, add stockfish support, edit gitignore

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import pygame
+import chess
+import chess.engine
 import sys
 
 # Configuration
 WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
+
+# Check Stockfish path
+if sys.platform == "win32":
+    STOCKFISH_PATH = "stockfish.exe"
+else:
+    STOCKFISH_PATH = "stockfish"
+
+def draw_board(screen):
+    colors = ["black","white"]
+    
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            color = colors[((row + col) % 2)]
+            pygame.draw.rect(screen, color, pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 def main():
     # Initialize Pygame and set up window
@@ -12,6 +30,10 @@ def main():
     pygame.display.set_caption("Player vs Stockfish")
     clock = pygame.time.Clock()
     
+    board = chess.Board()
+        
+    engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
+
     running = True
     
     # Main application loop
@@ -22,11 +44,12 @@ def main():
                 running = False
 
         # Rendering
-        screen.fill((50, 50, 50))
+        draw_board(screen)
         
         pygame.display.flip()
         clock.tick(MAX_FPS)
 
+    engine.quit()
     pygame.quit()
     sys.exit()
 
